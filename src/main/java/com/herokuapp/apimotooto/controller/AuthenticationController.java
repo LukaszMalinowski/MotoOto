c1ec7f21a55e09fb34ca12f188087e38fdd4fdda
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-08-11 20:45:47 +0200

    Refactored code

--- src/main/java/com/herokuapp/apimotooto/controller/AuthenticationController.java
+++ src/main/java/com/herokuapp/apimotooto/controller/AuthenticationController.java
@@ -2,17 +2,10 @@ package com.herokuapp.apimotooto.controller;
 
 import com.herokuapp.apimotooto.dto.UserCredentials;
 import com.herokuapp.apimotooto.dto.UserDto;
-import com.herokuapp.apimotooto.model.User;
-import com.herokuapp.apimotooto.service.UserService;
-import com.herokuapp.apimotooto.util.JwtUtil;
+import com.herokuapp.apimotooto.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,32 +18,18 @@ import javax.validation.Valid;
 @Slf4j
 @RequiredArgsConstructor
 public class AuthenticationController {
-
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtil jwtUtil;
-    private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping ("/login")
     public ResponseEntity<String> login(@RequestBody @Valid UserCredentials userCredentials) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            userCredentials.getEmail(), userCredentials.getPassword()
-                    ));
+        String accessToken = authenticationService.getAccessToken(userCredentials);
 
-            User user = (User)authentication.getPrincipal();
-            log.info(user.getEmail() + " logged in");
-            return ResponseEntity.ok(jwtUtil.createToken(user));
-        }
-        catch (BadCredentialsException exception) {
-            log.info(userCredentials.getEmail() + " entered bad credentials");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        return ResponseEntity.ok(accessToken);
     }
 
     @PostMapping ("/register")
     public ResponseEntity<String> register(@RequestBody @Valid UserDto userDto) {
-        userService.registerUser(userDto);
+        authenticationService.registerUser(userDto);
 
         return login(new UserCredentials(userDto.getEmail(), userDto.getPassword()));
     }
--- src/main/java/com/herokuapp/apimotooto/exception/handler/UserExceptionHandler.java
+++ src/main/java/com/herokuapp/apimotooto/exception/handler/UserExceptionHandler.java
@@ -3,6 +3,7 @@ package com.herokuapp.apimotooto.exception.handler;
 import com.herokuapp.apimotooto.exception.UserAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,6 +13,11 @@ public class UserExceptionHandler {
     @ExceptionHandler (value = UserAlreadyExistsException.class)
     public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler (value = BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsException(BadCredentialsException exception) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
 }
