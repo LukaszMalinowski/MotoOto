9f3086e3374c8b350fdfcacfdfe33143f35a6442
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-10-07 22:43:50 +0200

    Fixed adding identical announcements

--- src/main/java/com/herokuapp/apimotooto/repository/SaleAnnouncementRepository.java
+++ src/main/java/com/herokuapp/apimotooto/repository/SaleAnnouncementRepository.java
@@ -13,4 +13,7 @@ public interface SaleAnnouncementRepository extends JpaRepository<SaleAnnounceme
     //TODO add filtering by carMillage + check for sql injection
     @Query ("select s from SaleAnnouncement s where (:brand is null or (upper(s.car.brand) like upper(:brand))) and (:model is null or (upper(s.car.model) like upper(:model))) and (:generation is null or (upper(s.car.generation) like upper(:generation))) and (:fuel is null or (upper(s.car.fuelType) like upper(:fuel)))")
     List<SaleAnnouncement> findAllFilteredByCar(String brand, String model, String generation, String fuel);
+
+    boolean existsByCar_BrandAndCar_ModelAndOwner_Id(String brand, String model, Long userId);
 }
+
